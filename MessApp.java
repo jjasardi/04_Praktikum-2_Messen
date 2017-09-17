333d9029d0a1e606bb2f31e609f3d6e9e6a12eaf
Bernhard Tellenbach <tebe@zhaw.ch>
2017-09-17 17:06:26 +0200

    Initial commit of the source files.

---
+++ MessApp.java
@@ -0,0 +1,34 @@
+/**
+ * Die Klasse MessApp steuert einen Messablauf, um die Performance des
+ * Zufallszahlengenerators zu messen.
+ */
+public class MessApp {
+  private Messkonduktor messkonduktor;
+
+  /**
+   * Fuehrt eine Messung durch.
+   */
+  public void messen() {
+    initialisieren();
+    analyseDurchfuehren();
+    berechneUndDruckeMittelwerteMessreihe();
+    berechneUndDruckeMittelwerteMessung();
+  }
+
+  private void initialisieren() {
+    // TODO Objektsammlung und Messkonduktor erzeugen
+  }
+
+  private void analyseDurchfuehren() {
+    // TODO Benutzen Sie 'messkonduktor' um die Messungen
+    // durchzufuehren und in der Objektsammlung zu speichern.
+  }
+
+  private void berechneUndDruckeMittelwerteMessreihe() {
+    // TODO Implementieren Sie die Methode.
+  }
+
+  private void berechneUndDruckeMittelwerteMessung() {
+    // TODO Implementieren Sie die Methode.
+  }
+}
